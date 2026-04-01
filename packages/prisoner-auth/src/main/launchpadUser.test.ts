d9f77b974434fd525dc83b820caec1bc106c3117
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2026-04-01 16:46:33 +0100

    fix test

--- packages/prisoner-auth/src/main/launchpadUser.test.ts
+++ packages/prisoner-auth/src/main/launchpadUser.test.ts
@@ -4,20 +4,22 @@ import { userFromTokens } from './launchpadUser'
 describe('userFromTokens', () => {
   const aJwtOf = (obj: object): string => jwt.sign(obj, 'secret')
 
-  const idToken = aJwtOf({
+  const parsedIdToken = {
     name: 'Test User',
     given_name: 'Test',
     family_name: 'User',
     establishment: { agency_id: 'CKI' },
     sub: '12345',
-  })
+    iat: 1775000000, // not checked
+  }
+  const idToken = aJwtOf(parsedIdToken)
   const accessToken = aJwtOf({ hello: 'world' })
   const refreshToken = aJwtOf({ hello: 'world' })
 
   it('sets the given tokens on the user for later use (refreshing tokens)', () => {
     const result = userFromTokens({ idToken, accessToken, refreshToken })
 
-    expect(result.idToken).toEqual(idToken)
+    expect(result.idToken).toEqual(parsedIdToken)
     expect(result.accessToken).toEqual(accessToken)
     expect(result.refreshToken).toEqual(refreshToken)
   })
